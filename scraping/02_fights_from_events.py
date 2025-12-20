6cbe0eeadc0b475bc01f4afdbac600e99e04b568
Simonfox32 <simonfox131@gmail.com>
2025-12-20 10:32:09 -0700

    Add fight-level scraper and update gitignore

--- scraping/02_fights_from_events.py
+++ scraping/02_fights_from_events.py
@@ -11,28 +11,42 @@ EVENTS_HTML_DIR = ROOT / "data_raw"/ "html" / "events_pages"
 EVENTS_CSV_PATH = ROOT / "data_raw" / "extracts" / "events_master_list" / "all_events.csv"
 OUT_CSV_PATH = ROOT / "data_raw" / "extracts" / "all_fights" / "all_fights.csv"
 
+# Function that parses through an event, then adds all the fights of an event to an array
 def parse_events_fights(soup: BeautifulSoup) -> list[dict]:
     fights = []
     seen_url = set()
+    
+    # Iterates through every fight im event
     for a in soup.select('a[href*="fight-details"]'):
         fight_url = a.get('href', "").strip()
+        # Checks to see if fight URL exists, if it does continue (ensures no duplicate entries)
         if fight_url in seen_url:
             continue
         seen_url.add(fight_url)
         tr = a.find_parent('tr')
+        # Creates row of the entire fight then parses unique info
         if not tr:
             continue
         fighter_links = tr.select('a[href*="fighter-details"]')
         if len(fighter_links) < 2:
             continue
-        red_fighter_name = fighter_links[0].get_text(strip=True)
-        red_fighter_link = fighter_links[0].get('href', "").strip()
-        blue_fighter_link = fighter_links[1].get('href', "").strip()
-        blue_fighter_name = fighter_links[1].get_text(strip=True)
+        # Parses names and links from each fighter
+        fighter_1_name = fighter_links[0].get_text(strip=True)
+        fighter_1_link = fighter_links[0].get('href', "").strip()
+        fighter_2_name = fighter_links[1].get_text(strip=True)
+        fighter_2_link = fighter_links[1].get('href', "").strip()
         
+        # Converts row of data into a single string
         cells = [td.get_text(" ", strip = True) for td in tr.find_all('td')]
         row_texts = " | ".join(cells)
+        
+        # Saves outcome of the fight. Useful for knowing if a fight was win, NC, or draw
         outcome_marker = cells[0].lower() if len(cells) > 0 and cells[0] else None
+        
+        # End round and end time will be filled in later when fight details is parsed 
+        end_round = None
+        end_time_mmss = None
+        weight_class = None
         
         known_weights = [
             "Strawweight","Flyweight","Bantamweight","Featherweight","Lightweight",
@@ -40,10 +54,8 @@ def parse_events_fights(soup: BeautifulSoup) -> list[dict]:
             "Women's Strawweight","Women's Flyweight","Women's Bantamweight","Women's Featherweight",
             "Catch Weight"
         ]
-        end_round = None
-        end_time_mmss = None
-        weight_class = None
         
+        # Finds the weight class
         known_weights = sorted(known_weights, key=len, reverse=True)
         for w in known_weights:
             if w.lower() in row_texts.lower():
@@ -52,32 +64,39 @@ def parse_events_fights(soup: BeautifulSoup) -> list[dict]:
         
         method = None
         method_tokens = ['TKO', 'KO', 'SUB', 'U-DEC', 'S-DEC', 'M-DEQ', 'DQ', 'DEC', 'CNC', 'KO/TKO']
-        
+    
+        # Finds method of win
         for m in method_tokens:
             if m.lower() in row_texts.lower():
                 method = m
                 break
         
-
+        # Creates individual fight object 
         fights.append(
             {
-                "fight_url": fight_url,
-                "red_fighter_name": red_fighter_name,
-                "red_fighter_link": red_fighter_link,
-                "blue_fighter_name": blue_fighter_name,
-                "blue_fighter_link": blue_fighter_link,
+                "fighter_1_name": fighter_1_name,
+                "fighter_2_name": fighter_2_name,
                 "weight_class": weight_class,
                 "method": method,
                 "end_round": end_round,
                 "end_time_mmss": end_time_mmss,
                 "outcome_marker_raw": outcome_marker,
+                "fight_url": fight_url,
+                "fighter_1_link": fighter_1_link,
+                "fighter_2_link": fighter_2_link,
+                "fight_id" : id_from_url(fight_url),
+                "fighter_1_id": id_from_url(fighter_1_link),
+                "fighter_2_id": id_from_url(fighter_2_link),
                 "row_text_raw": row_texts
             }
         )
         
         
-        
+    # Returns list of every fight in an event
     return fights 
+def id_from_url(url: str) -> str:
+    id_ = url.split("/")[-1]
+    return id_
     
 def fetch_html(url: str) -> str: 
     headers = {
@@ -88,37 +107,50 @@ def fetch_html(url: str) -> str:
     return response.text
 
 def main(): 
+    # Event CSV is read
     df_events = pd.read_csv(EVENTS_CSV_PATH)
     all_fights = []
     i = 0
+    #Every row in df is iterated through
     for row in df_events.itertuples(index=False):
+        # Event information is saved
         event_name = row.event_name
         event_date = row.event_date
         event_url = row.event_url
         event_location = row.event_location
         EVENTS_HTML_DIR.parent.mkdir(parents=True, exist_ok=True)
+        # Events are given their own number to identify them
         Event_html_path = EVENTS_HTML_DIR / f"event_{i:04d}.html"
         i = i + 1
         
+        # Checks to see if an event already exists (redundancy)
         if Event_html_path.exists():
             html = Event_html_path.read_text(encoding='utf-8')
         else:
+        # If an event does not exist, save its HTML
             html = fetch_html(event_url)
             Event_html_path.write_text(html, encoding='utf-8')
             time.sleep(0.25)
         
+        # Sends HTML to be parsed 
         soup = BeautifulSoup(html, 'lxml')
         fights = parse_events_fights(soup)
+        
+        # Iterates through every fight in the event and adds event details
         for f in fights:
             f['event_name'] = event_name
             f['event_date'] = event_date
             f['event_location'] = event_location
             f['event_url'] = event_url
+            f['event_id'] = id_from_url(event_url)
         
+        # Creates a master list of every fight
         all_fights.extend(fights)
     
+    # Turns every fight into a dataframe
     df_fights = pd.DataFrame(all_fights).drop_duplicates(subset=['fight_url']).reset_index(drop=True)
     
+    # Turns the dataframe into a csv
     OUT_CSV_PATH.parent.mkdir(parents=True, exist_ok=True)
     df_fights.to_csv(OUT_CSV_PATH, index=False)
     print(df_fights.head())
---
+++ scraping/03_data_from_fights.py
@@ -0,0 +1,229 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+from pathlib import Path
+import time
+import requests
+import re
+import csv
+from typing import List, Dict, Any
+
+ROOT = Path(__file__).resolve().parents[1]
+ALL_FIGHTS_CSV = ROOT / "data_raw" / "extracts" / "all_fights" / "all_fights.csv"
+FIGHTS_HTML_DIR = ROOT / "data_raw" / "html" / "individual_fights"
+OUT_CSV_PATH = ROOT / "data_raw" / "extracts" / "rounds" / "rounds_parsed.csv"
+
+def load_done_fight_ids(csv_path: Path) -> set[str] :
+    if not csv_path.exists():
+        return set()
+    done = set()
+    with csv_path.open('r', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            done.add(row['fight_id'])
+    return done
+
+def get_fight_html(fight_id: str, fight_url: str) -> str:
+    path = FIGHTS_HTML_DIR / f"fight_{fight_id}.html"
+    if path.exists():
+        return path.read_text(encoding='utf-8')
+    html = fetch_html(fight_url)
+    path.write_text(html, encoding='utf-8')
+    return html
+    
+    
+def append_rows_to_csv(csv_path: Path, rows: List[Dict[str, any]]):
+    if not rows:
+        return
+    OUT_CSV_PATH.parent.mkdir(parents=True, exist_ok=True)
+    file_exists = csv_path.exists()
+    fieldnames = list(rows[0].keys())
+    with OUT_CSV_PATH.open("a", newline="", encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        if not file_exists:
+            writer.writeheader()
+        writer.writerows(rows)
+    
+def fetch_html(url: str) -> str: 
+    headers = {
+        "User-Agent" : "Mozilla/5.0"
+    }
+    response = requests.get(url, headers=headers, timeout=(10, 60))
+    response.raise_for_status()
+    return response.text
+
+def id_from_url(url: str) -> str:
+    id_ = url.split("/")[-1]
+    return id_
+
+def mmss_to_seconds(s: str) -> int:
+    s = (s or "").strip()
+    if ":" not in s:
+        return 0
+    m, sec = s.split(":", 1)
+    return int(m) * 60 + int(sec)
+
+
+def safe_parse(string: str):
+    try:
+        a,b = string.replace(" ", "").split("of")
+        return int(a), int(b)
+    except Exception: 
+        return 0, 0
+
+def safe_pct(landed: int, attempted: int):
+    try:
+        return landed/attempted
+    except Exception:
+        return 0.0
+
+def get_table_info(soup: BeautifulSoup):
+    round_rows = []
+    win_id = get_win_id(soup)
+    tables = soup.select('table')
+    if len(tables) < 2:
+        return []
+    round_table = tables[1]
+    rows = round_table.select("tbody tr")
+    for roundIndex, tr in enumerate(rows, start=1):
+        
+        tds = tr.select('td')
+        fighter_1, fighter_2 = get_pair_value(tds[0])  
+        kd_1, kd_2 = get_pair_value(tds[1])
+        
+        sigStrike_1, sigStrike_2 = get_pair_value(tds[2]) 
+        sig_landed_1, sig_att_1 = safe_parse(sigStrike_1)
+        sig_landed_2, sig_att_2 = safe_parse(sigStrike_2)
+        sig_pct_1= safe_pct(sig_landed_1, sig_att_1)
+        sig_pct_2= safe_pct(sig_landed_2, sig_att_2)
+
+
+        totalStrike_1, totalStrike_2 = get_pair_value(tds[4]) 
+        tot_landed_1, tot_att_1 = safe_parse(totalStrike_1)
+        tot_landed_2, tot_att_2 = safe_parse(totalStrike_2)
+        tot_pct_1= safe_pct(tot_landed_1, tot_att_1)
+        tot_pct_2= safe_pct(tot_landed_2, tot_att_2)
+
+        td_1, td_2 = get_pair_value(tds[5]) 
+        td_landed_1, td_att_1 = safe_parse(td_1)
+        td_landed_2, td_att_2 = safe_parse(td_2)
+        td_pct_1 = safe_pct(td_landed_1, td_att_1)
+        td_pct_2 = safe_pct(td_landed_2, td_att_2)
+        
+        subAttempt_1, subAttempt_2 = get_pair_value(tds[7])
+        reversal_1, reversal_2 = get_pair_value(tds[8])
+        ctrl_1, ctrl_2 = get_pair_value(tds[9])
+        ctrl_1 = mmss_to_seconds(ctrl_1)
+        ctrl_2 = mmss_to_seconds(ctrl_2)
+        round_rows.append({
+            
+            "round": roundIndex,
+            "fighter_1": fighter_1,
+            "fighter_2":fighter_2,
+            
+            "kd_1": int(kd_1) if kd_1.isdigit() else 0,
+            "kd_2": int(kd_2) if kd_2.isdigit() else 0,
+            
+            "sig_landed_1": sig_landed_1,
+            "sig_att_1":  sig_att_1,
+            "sig_pct_1": round(sig_pct_1, 4),
+            
+            "sig_landed_2": sig_landed_2,
+            "sig_att_2":  sig_att_2,
+            "sig_pct_2": round(sig_pct_2, 4),
+            
+            "tot_landed_1": tot_landed_1,
+            "tot_att_1":  tot_att_1,
+            "tot_pct_1": round(tot_pct_1, 4),
+            
+            "tot_landed_2": tot_landed_2,
+            "tot_att_2":  tot_att_2,
+            "tot_pct_2": round(tot_pct_2, 4),
+            
+            "td_landed_1": td_landed_1,
+            "td_att_1": td_att_1,
+            "td_pct_1": round(td_pct_1, 4),
+            
+            "td_landed_2": td_landed_2,
+            "td_att_2": td_att_2,
+            "td_pct_2": round(td_pct_2, 4),
+            
+            "sub_1": int(subAttempt_1) if subAttempt_1.isdigit() else 0,
+            "sub_2": int(subAttempt_2) if subAttempt_2.isdigit() else 0,
+            
+            
+            "rev_1": int(reversal_1) if reversal_1.isdigit() else 0,
+            "rev_2": int(reversal_2) if reversal_2.isdigit() else 0,
+            
+            "ctrl_sec_1": ctrl_1,
+            "ctrl_sec_2": ctrl_2,
+            "win_id": win_id
+            
+        })
+    return round_rows
+    
+    
+
+
+    
+def get_pair_value(td):
+    parts = [x.strip() for x in td.get_text("\n", strip=True).split("\n") if x.strip()]
+    v_1 = parts[0] if len(parts) > 0 else ""
+    v_2 = parts[1] if len(parts) > 1 else ""
+    # print(v_1)
+    # print(v_2)
+    return v_1, v_2
+    
+    
+def get_detail_value(soup: BeautifulSoup, label: str) -> str:
+    items=soup.select(".b-fight-details__text-item")
+    for item in items:
+        txt = " ".join(item.get_text(" ", strip=True).replace("\xa0", " ").split())
+        if txt.lower().startswith(label.lower() + ":"):
+            return txt.split(":", 1)[1].strip()
+    return None
+    
+def get_win_id(soup: BeautifulSoup) -> str:
+    win_marker = soup.select_one("i.b-fight-details__person-status_style_green")
+    if not win_marker:
+        return None
+    
+    win_container = win_marker.find_parent('div')
+    if not win_container:
+        return None
+    
+    win_link = win_container.select_one('a[href*="fighter-details"]')
+    if not win_link:
+        return None
+    
+    return(id_from_url(win_link['href']))
+
+def main() :
+    df_fights = pd.read_csv(ALL_FIGHTS_CSV)
+    FIGHTS_HTML_DIR.mkdir(parents=True, exist_ok=True)
+    done_ids = load_done_fight_ids(OUT_CSV_PATH)
+    for row in df_fights.itertuples(index=False):
+        fight_id = id_from_url(row.fight_url)
+        
+        if fight_id in done_ids:
+            continue
+        
+        fight_url = row.fight_url
+        fighter_1_url = row.fighter_1_link
+        fighter_2_url = row.fighter_2_link
+        fighter_1_id = id_from_url(fighter_1_url)
+        fighter_2_id = id_from_url(fighter_2_url)
+        event_id = id_from_url(row.event_url)
+        
+        
+        html = get_fight_html(fight_id, fight_url)
+        soup = BeautifulSoup(html, "lxml")
+        fight = get_table_info(soup)
+        meta = ({"event_id": event_id, "fight_id":fight_id, "fighter_1_id":fighter_1_id, "fighter_2_id":fighter_2_id})
+        for row in fight:
+            row.update(meta)
+        append_rows_to_csv(OUT_CSV_PATH, fight)
+        done_ids.add(fight_id)
+
+
+if __name__ == "__main__" :
+    main()
